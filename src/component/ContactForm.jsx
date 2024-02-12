7b792ff003fd46d334186ac9b621be9820fe427a
pantherforce <likhithreddy150@gmail.com>
2024-02-13 01:47:45 +0530

    Revert "final-push and update"
    This reverts commit 1c7734e06cad6e4086924c8b3578b6966f9df4e8.

--- src/component/ContactForm.jsx
+++ src/component/ContactForm.jsx
@@ -4,9 +4,9 @@ import React from 'react';
 const ContactForm = () => {
     return (
         <div className='flex justify-center mb-10 items-center'>
-            <iframe className='w-3/4'
+            <iframe
                 src="https://docs.google.com/forms/d/e/1FAIpQLSeQtjjMra1Lmmh946FlJTYjxwZzp_ktSKKJ8d9RfyMmaxU4SQ/viewform?embedded=true"
-
+                width="640"
                 height="303"
                 frameBorder="0"
                 marginHeight="0"
